a6b8b1162baf655c505fda20e86e9c8947489c97
San100Ferrari <67142061+San100Ferrari@users.noreply.github.com>
2020-07-01 11:45:44 +0200

    Add files via upload

---
+++ Exo_2js.js
@@ -0,0 +1,29 @@
+//<!DOCTYPE html>
+//<html lang="fr">
+    //<head>
+        //<meta charset="utf-8">
+        //<title>Exo 2 JS</title>
+    //</head>
+    //<body>
+        //<h1>Exercice 2 : les variables</h1>
+        //<p>Les différents types de variables</p>
+    //</body>
+//</html>
+
+//Dans cet exercice vous allez créer des variables de types :
+//- numérique (positifs, négatifs ou à virgule)
+//- chaines de caractères
+//- booléens
+
+//Affichez les ensuite dans la console avec leur type (fonction typeof()).
+
+let numberOne = 1;
+let nbNegativeOne = -1;
+let nbFloat = 1.1;
+let stringVar = "number";
+let booleansOne = Boolean(false);
+typeof(numberOne);
+typeof(nbNegativeOne);
+typeof(nbFloat);
+typeof(stringVar);
+typeof(booleansOne);
