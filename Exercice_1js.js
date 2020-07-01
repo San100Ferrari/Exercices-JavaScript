e5a72924c5c5888fdaf24c634693bfdfd64f1fca
San100Ferrari <67142061+San100Ferrari@users.noreply.github.com>
2020-07-01 11:27:12 +0200

    Add files via upload

---
+++ Exercice_1js.js
@@ -0,0 +1,8 @@
+alert("Hello World!");
+
+//<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8"><title>Exo 1 JS</title><script type="text/javascript" src="Exercice_1js.js"></script></head>
+    //<body>
+        //<h1>Exercice 1 : Ma premiere page avec du JS</h1>
+        //<p>Le message hello world!</p>
+    //</body>
+//</html>
