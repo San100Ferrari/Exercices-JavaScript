e1ec062895a12a970d4ac7bdc86f3da620c8547d
San100Ferrari <67142061+San100Ferrari@users.noreply.github.com>
2020-07-02 10:15:15 +0200

    Add files via upload

---
+++ Exo-3js.js
@@ -0,0 +1,27 @@
+//Retour à l'école , le prof vous donne  les éléments suivants :
+//a = 12 , b = 25 , c = 3
+//affichez les résultats des opérations suivantes dans la console:
+
+//a + b
+//a * b
+//a / c
+//a % c
+
+//Maintenant que vous êtes un supercalculateur, vous pouvez chercher la réponse à 
+//La Grande Question sur la vie, l'univers et le reste :
+//(a+c)*c/b*10+a*2
+
+let a = 12;
+let b = 25;
+let c = 3;
+let r1 = a + b;
+console.log(r1);
+let r2 = a * b;
+console.log(r2);
+let r3 = a / c;
+console.log(r3);
+let r4 = a % c;
+console.log(r4);
+let finalResult = (a+c)*c/b*10+a*2;
+console.log(finalResult);
+//42
