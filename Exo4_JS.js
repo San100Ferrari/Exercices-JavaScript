7e2d3aa02eb5541ac61d9b0541f8dab881c24168
San100Ferrari <67142061+San100Ferrari@users.noreply.github.com>
2020-08-17 14:35:39 +0200

    Add files via upload

---
+++ Exo4_JS.js
@@ -0,0 +1,19 @@
+let a = 2;
+let b = "2";
+let c = 14;
+let d = 1 + "4";
+
+let e = (a==b);
+console.log(e);
+
+let f = (a!==b);
+console.log(f);
+
+let g = (c>=a);
+console.log(g);
+
+let h = (c!=d);
+console.log(h);
+
+let i = (c===d);
+console.log(i);
