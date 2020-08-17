6a72391a42e95f1cf3a1d7e45464905d69373a13
San100Ferrari <67142061+San100Ferrari@users.noreply.github.com>
2020-08-17 10:17:29 +0200

    Add files via upload

---
+++ Exo3_JS.js
@@ -0,0 +1,22 @@
+let a = 12;
+let b = 25;
+let c = 3;
+
+//test1
+console.log(a);
+
+//Exo
+let r1 = a + b;
+console.log(r1);
+
+let r2 = a * b;
+console.log(r2);
+
+let r3 = a / c;
+console.log(r3);
+
+let r4 = a % c;
+console.log(r4);
+
+let f = (a+c)*c/b*10+a*2;
+console.log(f);
